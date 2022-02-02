fab5af6a675f62eea9826b109811c9035986bd94
Briton Dunn <dunnbriton@unm.edu>
2022-02-01 23:35:23 -0700

    - Actually created/added .jar. - Commented out a comment outputing the board. - Replace seed with random value.

--- src/MainGameLoop.java
+++ src/MainGameLoop.java
@@ -2,7 +2,7 @@ public class MainGameLoop {
 
     public static void main(String[] args){
         Board.Manager boardOne = new Board.Manager(4,5);
-        boardOne.fill(100);
+        boardOne.fill((int) Math.random());
         Display.main();
         //System.out.println(boardOne);
     }
